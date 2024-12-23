58a2cdf5c17f1e69b5e6a6568bc1b2309ee2e8f9
ericavidaloop <vidalerica39@gmail.com>
2024-12-23 15:20:16 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Create a Circle object
-        Circle circle = new Circle(9.0, "green", true);
-        System.out.println("Circle: " + circle.toString());
-        System.out.println("Area: " + circle.getArea());
-        System.out.println("Perimeter: " + circle.getPerimeter());
+        Shape circle = new Circle(9.0, "orange", true);
+        Shape rectangle = new Rectangle(5.0, 7.0, "green", false);
+        Shape square = new Square(6.0, "blue", true);
 
-        // Create a Rectangle object
-        Rectangle rectangle = new Rectangle(5.0, 6.0, "orange", false);
-        System.out.println("\nRectangle: " + rectangle.toString());
-        System.out.println("Area: " + rectangle.getArea());
-        System.out.println("Perimeter: " + rectangle.getPerimeter());
+        System.out.println(circle);
+        System.out.println();
+        System.out.println(rectangle);
+        System.out.println();
+        System.out.println(square);
     }
-  }
+}
